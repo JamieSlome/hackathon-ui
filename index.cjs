eddcbed4e870ddcc524a0c56293c98b5de7ecf2f
Dan A <dcatoday-cloud9hack>
2023-09-21 16:56:41 -0500

    update server

--- index.cjs
+++ index.cjs
@@ -6,6 +6,10 @@ const app = express()
 app.use(express.static(path.join(".", 'dist'))); 
 
 
-app.listen("3000", () => {
+app.listen("8080", () => {
     console.log("heya")
+})
+
+app.get("*", (req, res) => {
+    res.redirect('/index.html');
 })
