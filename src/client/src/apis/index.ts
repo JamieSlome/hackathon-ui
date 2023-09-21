f8ffcf009b36fb1757754a06482d89c27afdac33
Zac Sutton <zacharydsutton@gmail.com>
2023-09-21 15:32:49 -0700

    add client

---
+++ src/client/src/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BeneficiaryApi';
+export * from './NeedApi';
+export * from './OrganizationApi';
---
+++ src/client/src/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Beneficiary';
+export * from './BeneficiaryNeed';
+export * from './Need';
+export * from './Organization';
